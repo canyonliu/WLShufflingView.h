1d2538a6a68168410b85588623941724ed6ea9ca
刘清灿 <lqc_loadingu@163.com>
2016-06-01 14:13:18 +0800

    Initial Commit

---
+++ WLShuffling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WLShuffling
+//
+//  Created by weixvn on 16/6/1.
+//  Copyright © 2016年 weixvn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WLShuffling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WLShuffling
+//
+//  Created by weixvn on 16/6/1.
+//  Copyright © 2016年 weixvn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
